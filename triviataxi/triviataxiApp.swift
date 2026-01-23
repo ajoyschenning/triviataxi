0299237939b2f3fa8a207b3c67232f46c4da3d51
Alex Joy Schenning <alexandra.m.joy.schenning@vanderbilt.edu>
2026-01-23 15:05:13 -0600

    Initial Commit

---
+++ triviataxi/triviataxiApp.swift
@@ -0,0 +1,17 @@
+//
+//  triviataxiApp.swift
+//  triviataxi
+//
+//  Created by Alex Joy Schenning on 1/23/26.
+//
+
+import SwiftUI
+
+@main
+struct triviataxiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
