f43483cb06f74af01445b53ae4087ff00c7d4c19
camikrugel <112404317+camikrugel@users.noreply.github.com>
2026-01-23 19:42:20 -0600

    Working firebase connection

--- triviataxi/triviataxiApp.swift
+++ triviataxi/triviataxiApp.swift
@@ -6,9 +6,23 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+// 1. Create the "Switchboard" (AppDelegate)
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // This is where we turn the key to start Firebase
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct triviataxiApp: App {
+    // 2. Tell SwiftUI to use our Switchboard
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
